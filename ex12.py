e3179cb787c780c70d209e1b2a61b672e21284eb
Mustafa Kamil <godric.phoenix@gmail.com>
2019-11-20 19:27:35 -0600

    add ex12

---
+++ ex12.py
@@ -0,0 +1,9 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# to get more information on input() go to the terminal and put
+# $ pydoc input
+# if you done with reading please click on q
